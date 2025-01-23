bd6b38768954961a90e82e88a0bbc7f71d9ad000
Alexsandro Mendes <alexsandroferreiramendes00@gmail.com>
2025-01-23 13:54:27 -0300

    feat(utils): Adiciona funções para adicionar feriados municipais e estaduais   - append_feriado_estado: Adiciona um feriado estadual ao banco de dados.   - append_feriado_municipios: Adiciona um feriado municipal ao banco de dados.
    fix(utils): Correção e melhoria nas funções de Pegar feriados pelo id do municipio e pelo id do estado
      - get_feriado_estado: busca no banco de dados e retorna o feriado referente a data e o codifo ibge recebidos.
      - get_feriado_municipal: busca no banco de dados e retorna o feriado referente a data e o codifo ibge recebidos.
    
    refactor(utils): Renomeia 'feriados_fixos' para 'feriadosNacionais' e refatora 'criar_banco'
      - Renomeia a função 'feriados_fixos' para 'feriadosNacionais' para melhor refletir seu significado.
      - A função 'criar_banco' não recebe mais o ano.
    
    fix(utils): Remove feriados móveis da lista de feriados nacionais
      - Remove os feriados móveis da lista de feriados nacionais.

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+from fastapi import status
+
+from sqlalchemy.orm.attributes import flag_modified
+
 from models import Estado as ModelEstado 
 from models import Municipio as ModelMunicipo
 
@@ -7,7 +11,7 @@ from feriados import FeriadosMoveis
 
 import csv
 
-def feriados_fixos():
+def feriados_nacionais():
     """
     Cria um dicionário com os feriados fixos de todos os anos.
 
@@ -63,7 +67,7 @@ def carregar_dados(arquivo_csv, ano):
             if not estado_existente:
                 estado = ModelEstado(
                     codigo_ibge=row["codigo_ibge"][0:2],
-                    feriados_nacionais= feriados_moveis(ano) | feriados_fixos(),
+                    feriados_nacionais=feriados_nacionais(),
                     )
 
                 sessionLocal.add(estado)
@@ -79,8 +83,7 @@ def carregar_dados(arquivo_csv, ano):
             sessionLocal.commit()
 
 
-
-def criar_banco(arquivo, ano):
+def criar_banco(arquivo):
     """
     Verifica se o banco de dados já foi criado. Se não, ele cria o banco de dados.
 
@@ -94,46 +97,115 @@ def criar_banco(arquivo, ano):
     dados = sessionLocal.query(ModelEstado).all()
 
     if not dados:
-        carregar_dados(arquivo , ano)
+        carregar_dados(arquivo)
         return 'banco de dados criado'
     return 'banco de dados já exite.'
     
 
 def get_feriado_municipio(codigo_ibge, data):
     """
-    Retorna o feriado para um município específico em uma data.
+        Retorna o feriado para um município específico em uma data.
 
-    Args:
-        codigo_ibge (str): Código IBGE do município.
-        data (str): Data no formato MM-DD.
+        Args:
+            codigo_ibge (str): Código IBGE do município.
+            data (str): Data no formato MM-DD.
 
-    Returns:
-        dict: Dicionário contendo o nome do feriado,
+        Returns:
+            dict: Dicionário contendo o nome do feriado,
     """
 
 
     municipio = sessionLocal.query(ModelMunicipo).filter_by(codigo_ibge=codigo_ibge).first()
-    if municipio.estado.feriados_estaduais:
-        feriados = municipio.estado.feriados_nacionais | municipio.estado.feriados_estaduais
-        return feriados.get(data)
+    estado = municipio.estado
+    if estado.feriados_estaduais:
+        if municipio.feriados_municipais:
+            feriados = estado.feriados_nacionais | estado.feriados_estaduais | municipio.feriados_municipais
+        
+            return feriados.get(data)
     
-    else:
-        feriados = municipio.estado.feriados_nacionais
+        feriados = estado.feriados_nacionais | estado.feriados_estaduais 
         return feriados.get(data)
 
+    if municipio.feriados_municipais:
+        feriados = estado.feriados_nacionais | municipio.feriados_municipais
+        
+        return feriados.get(data)
+    
+    feriados = estado.feriados_nacionais
+
+    print(feriados)
+    return feriados.get(data)
 
 def get_feriado_estado(codigo_ibge, data):
     """
-    Retorna o feriado para um estado específico em uma data.
+        Retorna o feriado para um estado específico em uma data.
 
-    Args:
-        codigo_ibge (str): Código IBGE do estado.
-        data (str): Data no formato MM-DD.
+        Args:
+            codigo_ibge (str): Código IBGE do estado.
+            data (str): Data no formato MM-DD.
 
-    Returns:
-        dict: Dicionário contendo o nome do feriado.
+        Returns:
+            dict: Dicionário contendo o nome do feriado.
     """
     estado = sessionLocal.query(ModelEstado).filter_by(codigo_ibge=codigo_ibge).first()
-    feriados = estado.feriados_nacionais
-    return feriados[data]
+    if estado.feriados_estaduais:
+        feriados = estado.feriados_nacionais | estado.feriados_estaduais
+        return feriados.get(data)
+    
+    else:
+        feriados = estado.feriados_nacionais
+        return feriados.get(data)
 
+
+
+def append_feriado_estado(codigo_ibge: str, data: str, feriado: dict):
+    estado = sessionLocal.query(ModelEstado).filter_by(codigo_ibge=codigo_ibge).first()
+
+
+    if estado:
+        if estado.feriados_estaduais:  
+
+            if estado.feriados_estaduais.get(data):
+                estado.feriados_estaduais[data]["name"] = feriado["name"]
+                response = status.HTTP_200_OK
+
+            else:
+                estado.feriados_estaduais.update({data: feriado})
+                response = status.HTTP_201_CREATED
+
+        else:
+            estado.feriados_estaduais = {data:feriado}
+            response = status.HTTP_201_CREATED
+        
+        flag_modified(estado, "feriados_estaduais")
+
+        sessionLocal.commit()
+        sessionLocal.close()
+        return "complet", response
+
+    else:
+        return estado
+
+def append_feriado_municipal(codigo_ibge: str, data: str, feriado:dict):
+    municipio = sessionLocal.query(ModelMunicipo).filter_by(codigo_ibge=codigo_ibge).first()
+
+    if municipio:
+        if municipio.feriados_municipais:
+            if municipio.feriados_municipais.get(data):
+                municipio.feriados_municipais[data]["name"] = feriado["name"]
+                reponse = status.HTTP_200_OK
+
+            else:
+                municipio.feriados_municipais.update({data, feriado})
+                reponse =  status.HTTP_201_CREATED
+        else:
+            municipio.feriados_municipais = {data: feriado}
+            reponse = status.HTTP_201_CREATED
+
+        flag_modified(municipio, "feriados_municipais")
+        sessionLocal.commit()
+        sessionLocal.close()
+        return "complet",reponse
+    
+    else:
+        return None,municipio
